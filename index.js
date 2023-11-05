c8589bd2dc0bcf4b56f247d68f8ea873d2df7df9
ankitchoudhary8120@gmail.com <ankitchoudhary8120@gmail.com>
2023-11-05 09:00:10 +0530

    removie button is completed

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var isStatus = document.querySelector("h5");
-
-
+var RemoveFriend = document.querySelector("#remove")
 var addFriend = document.querySelector("#add");
-
 
 addFriend.addEventListener("click", function () {
     isStatus.innerHTML = "Friends"
+    isStatus.style.color = 'green'
+})
+RemoveFriend.addEventListener("click", function () {
+    isStatus.innerHTML = "Strenger"
+    isStatus.style.color = 'red'
 })
