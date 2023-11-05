7f497f55e7183188d6dd2cf2090db69494539fd4
ankitchoudhary8120@gmail.com <ankitchoudhary8120@gmail.com>
2023-11-05 08:51:29 +0530

    Add Friend completed

---
+++ index.js
@@ -0,0 +1,9 @@
+var isStatus = document.querySelector("h5");
+
+
+var addFriend = document.querySelector("#add");
+
+
+addFriend.addEventListener("click", function () {
+    isStatus.innerHTML = "Friends"
+})
