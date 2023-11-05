ec5b0f05ee3361ff8c26d9c8e17c70ff2e307197
ankitchoudhary8120@gmail.com <ankitchoudhary8120@gmail.com>
2023-11-05 09:11:59 +0530

    Add Friend Project is complete

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 var isStatus = document.querySelector("h5");
-var RemoveFriend = document.querySelector("#remove")
-var addFriend = document.querySelector("#add");
+var btn = document.querySelector("#add");
 
-addFriend.addEventListener("click", function () {
-    isStatus.innerHTML = "Friends"
-    isStatus.style.color = 'green'
-})
-RemoveFriend.addEventListener("click", function () {
-    isStatus.innerHTML = "Strenger"
-    isStatus.style.color = 'red'
+var check = 0;
+
+btn.addEventListener("click", function () {
+    if (check == 0) {
+        isStatus.innerHTML = "Friends"
+        isStatus.style.color = 'green'
+        btn.innerHTML = "Remove Friend"
+        check = 1;
+    } else {
+        isStatus.innerHTML = "Stranger"
+        isStatus.style.color = "red"
+        btn.innerHTML = "Add Friend"
+        check = 0;
+    }
+
 })
